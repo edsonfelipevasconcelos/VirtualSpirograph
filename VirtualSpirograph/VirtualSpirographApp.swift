f32c62af19ca5a4ee9ea08ea9ec9520564511c28
Edson Felipe Vasconcelos <edsonfelipe@gmail.com>
2023-05-08 23:46:45 -0300

    Initial Commit

---
+++ VirtualSpirograph/VirtualSpirographApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualSpirographApp.swift
+//  VirtualSpirograph
+//
+//  Created by EDSON FELIPE VASCONCELOS on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct VirtualSpirographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
